b849cab16474aaf677fef10657414e4c0dd6990d
Luiza Nurnberg <nurnbergluiza@gmail.com>
2023-04-25 17:01:05 -0300

    style: ajustada tipagem dos métodos startServer e publishLetter

--- src/services/LetterService.ts
+++ src/services/LetterService.ts
@@ -2,7 +2,7 @@ import { LetterProducer } from "./messageBroker/LetterProducer";
 import { Request } from "express";
 
 export class LetterService {
-    public publishLetter(letter: Request) {
+    public publishLetter(letter: Request): void {
         const letterProducer: LetterProducer = new LetterProducer();
         letterProducer.sendMessageToQueue(letter);
     }
